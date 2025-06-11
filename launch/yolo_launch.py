bd1fb42cf73e05e56667018d71d8ae6429524b07
leonardonels <goldenmatrix2.0@gmail.com>
2025-06-11 17:10:07 +0200

    push

--- launch/yolo_launch.py
+++ launch/yolo_launch.py
@@ -6,12 +6,19 @@ def generate_launch_description():
 
     pkg_dir = os.path.dirname(os.path.abspath(__file__))
     cone_detection = os.path.join(pkg_dir, '../cone_detection/cone_detection_node.py')
+    pointcloud_filter = os.path.join(pkg_dir, '../pointcloud_filter/pointcloud_filter_node.py')
 
     cone_detectio_node = ExecuteProcess(
         cmd=['python3', cone_detection],
         output='screen'
     )
 
+    pointcloud_filter_node = ExecuteProcess(
+        cmd=['python3', pointcloud_filter],
+        output='screen'
+    )
+
     return LaunchDescription([
-        cone_detectio_node
+        cone_detectio_node,
+        #pointcloud_filter_node
     ])
